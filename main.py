de76929675725383a1016d383bc2eb638de6d932
pwu97 <43555461+pwu97@users.noreply.github.com>
2021-05-12 02:36:21 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, session, redirect, Response, url_for, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/projects')
+def projects_page():
+    return render_template('projects.html')
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host = '127.0.0.1', port = 6060, debug = True)
