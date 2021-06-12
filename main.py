1c103b4c97048b567cae470b4a0e85aa209b6de5
pwu97 <43555461+pwu97@users.noreply.github.com>
2021-06-12 01:07:06 -0400

    Update some stuff on the site.

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from flask import Flask, request, render_template, session, redirect, Response, 
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/problems')
+def problems():
+    return render_template('problems.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
 
 @app.route('/projects')
-def projects_page():
+def projects():
     return render_template('projects.html')
 
 @app.errorhandler(500)
@@ -19,6 +27,4 @@ def server_error(e):
     """.format(e), 500
 
 if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host = '127.0.0.1', port = 6060, debug = True)
+    app.run(host = '127.0.0.1', debug = True)
