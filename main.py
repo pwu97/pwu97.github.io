c29aea5295294f4861e2ff0f4a56c0d0eabc01c6
pwu97 <peterwu@andrew.cmu.edu>
2021-11-20 03:21:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,57 +3,57 @@ from flask import Flask, request, render_template, session, redirect, Response, 
 app = Flask(__name__)
 
 @app.route('/')
-def index_page():
+def index():
     return render_template('index.html')
 
 @app.route('/data')
-def data_page():
+def data():
     return render_template('data.html')
 
 @app.route('/problems')
-def problems_page():
-    problems = [
-        {'lc_link': 'https://leetcode.com/problems/group-anagrams/',
-         'sol_link': '',
-         'name': 'Group Anagrams',
-         'company': 'amazon',
-         'number': 49,
-         'difficulty': 'Medium',
-         'topics': 'Hash Table, String, Sorting'},
-        {'lc_link': 'https://leetcode.com/problems/valid-number/',
-         'sol_link': '',
-         'name': 'Valid Number',
-         'company': 'fb',
-         'number': 65,
-         'difficulty': 'Hard',
-         'topics': 'String'},
-        {'lc_link': 'https://leetcode.com/problems/valid-palindrome/',
-         'sol_link': '',
-         'name': 'Valid Palindrome',
-         'company': 'spotify',
-         'number': 125,
-         'difficulty': 'Easy',
-         'topics': 'Two Pointers, String'},
-        {'lc_link': 'https://leetcode.com/problems/happy-number/',
-         'sol_link': '',
-         'name': 'Happy Number',
-         'company': 'twitter',
-         'number': 202,
-         'difficulty': 'Easy',
-         'topics': 'Hash Table, Math, Two Pointers'},
-        {'lc_link': 'https://leetcode.com/problems/find-median-from-data-stream/',
-         'sol_link': '',
-         'name': 'Find Median from Data Stream',
-         'company': 'microsoft',
-         'number': 295,
-         'difficulty': 'Hard',
-         'topics': 'Two Pointers, Design, Sorting, Heap, Data Stream'},
-    ]
+def problems():
+#    problems = [
+#        {'lc_link': 'https://leetcode.com/problems/group-anagrams/',
+#         'sol_link': '',
+#         'name': 'Group Anagrams',
+#         'company': 'amazon',
+#         'number': 49,
+#         'difficulty': 'Medium',
+#         'topics': 'Hash Table, String, Sorting'},
+#        {'lc_link': 'https://leetcode.com/problems/valid-number/',
+#         'sol_link': '',
+#         'name': 'Valid Number',
+#         'company': 'fb',
+#         'number': 65,
+#         'difficulty': 'Hard',
+#         'topics': 'String'},
+#        {'lc_link': 'https://leetcode.com/problems/valid-palindrome/',
+#         'sol_link': '',
+#         'name': 'Valid Palindrome',
+#         'company': 'spotify',
+#         'number': 125,
+#         'difficulty': 'Easy',
+#         'topics': 'Two Pointers, String'},
+#        {'lc_link': 'https://leetcode.com/problems/happy-number/',
+#         'sol_link': '',
+#         'name': 'Happy Number',
+#         'company': 'twitter',
+#         'number': 202,
+#         'difficulty': 'Easy',
+#         'topics': 'Hash Table, Math, Two Pointers'},
+#        {'lc_link': 'https://leetcode.com/problems/find-median-from-data-stream/',
+#         'sol_link': '',
+#         'name': 'Find Median from Data Stream',
+#         'company': 'microsoft',
+#         'number': 295,
+#         'difficulty': 'Hard',
+#         'topics': 'Two Pointers, Design, Sorting, Heap, Data Stream'},
+#    ]
     
     return render_template('problems.html')
 
 @app.route('/projects')
-def projects_page():
+def projects():
     return render_template('projects.html')
 
 @app.errorhandler(500)
